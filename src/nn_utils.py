0607d69c5f303dd2a5d4dbadf78514262737e93f
Suraj Rohira <sr01303@surrey.ac.uk>
2023-11-30 20:20:14 +0000

    started sam

--- src/nn_utils.py
+++ src/nn_utils.py
@@ -14,8 +14,6 @@ DEVICE = (
     else "cpu"
     )
 
-torch.cuda.empty_cache()
-
 '''
     Load the Dataset
 '''
@@ -50,74 +48,38 @@ TESTLOADER = torch.utils.data.DataLoader(
 '''
     Define the Neural Network Architecture
 '''
-def conv_block(in_channels, out_channels, pool=False):
-    layers = [
-        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
-        nn.BatchNorm2d(out_channels), 
-        nn.ReLU(inplace=True)
-        ]
-    
-    if pool:
-        layers.append(nn.MaxPool2d(2))
-    
-    return nn.Sequential(*layers)  
-
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
         
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
         self.batchNorm1 = nn.BatchNorm2d(64)
-        self.relu1 = nn.ReLU()
 
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
         self.batchNorm2 = nn.BatchNorm2d(128)
-        self.relu2 = nn.ReLU()
-        self.maxPool1 = nn.MaxPool2d(kernel_size=2)
 
         # residual block start
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
         self.batchNorm3 = nn.BatchNorm2d(128)
-        self.relu3 = nn.ReLU()
-
         self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
         self.batchNorm4 = nn.BatchNorm2d(128)
-        self.relu4 = nn.ReLU()
-        # residual block end
 
         self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
         self.batchNorm5 = nn.BatchNorm2d(256)
-        self.relu5 = nn.ReLU()
-        self.maxPool2 = nn.MaxPool2d(kernel_size=2)
 
         self.conv6 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1)
         self.batchNorm6 = nn.BatchNorm2d(512)
-        self.relu6 = nn.ReLU()
-        self.maxPool3 = nn.MaxPool2d(kernel_size=2)
 
         # residual block start
         self.conv7 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
         self.batchNorm7 = nn.BatchNorm2d(512)
-        self.relu7 = nn.ReLU()
-
         self.conv8 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
         self.batchNorm8 = nn.BatchNorm2d(512)
-        self.relu8 = nn.ReLU()
         # residual block end        
         
         self.out = nn.Linear(512, 10)
     
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-
-        x = self.res1(x) + x
-        
-        x = self.conv3(x)
-        x = self.conv4(x)
-        
-        x = self.res2(x) + x
-
         # first block
         x = self.conv1(x)
         x = self.batchNorm1(x)
@@ -130,40 +92,40 @@ class Net(nn.Module):
         x = F.max_pool2d(x, kernel_size=2)
 
         # residual block
-        x_res1 = F.max_pool2d(x, kernel_size=2) # save for residual block
+        x_res1 = x
         x = self.conv3(x)
         x = self.batchNorm3(x)
         x = F.relu(x)
-
         x = self.conv4(x)
         x = self.batchNorm4(x)
         x = F.relu(x)
-
         x = x + x_res1
         
         # third block
         x = self.conv5(x)
         x = self.batchNorm5(x)
         x = F.relu(x)
-        x = F.max_pool2d(kernel_size=2)
+        x = F.max_pool2d(x, kernel_size=2)
 
+        # fourth block
         x = self.conv6(x)
         x = self.batchNorm6(x)
         x = F.relu(x)
-        x = F.max_pool2d(kernel_size=2)
+        x = F.max_pool2d(x, kernel_size=2)
 
         # residual block
+        x_res2 = x
         x = self.conv7(x)
         x = self.batchNorm7(x)
         x = F.relu(x)
-
         x = self.conv8(x)
         x = self.batchNorm8(x)
         x = F.relu(x)
+        x = x + x_res2
         
+        # output 
         x = F.max_pool2d(x, kernel_size=4)
         x = torch.flatten(x, 1) # flatten all dimensions except batch
-        
         x = self.out(x)
 
         return x
@@ -179,11 +141,12 @@ def train_nn(net: nn.Module, epochs: int, optimizer: torch.optim.Optimizer):
     print(f'- Optimiser: {optimizer}')
     print(f'- Loss function: {F.cross_entropy.__name__}')
 
-    accuracy_track = []
-    loss_track = []
+    accuracy_per_epoch_track = []
+    loss_per_epoch_track = []
 
     # loop over the dataset multiple times
     for epoch in range(epochs):
+        running_loss = 0
         # loop over the dataset by mini-batch
         for mini_batch in TRAINLOADER:
             images = mini_batch[0].to(DEVICE)
@@ -196,26 +159,28 @@ def train_nn(net: nn.Module, epochs: int, optimizer: torch.optim.Optimizer):
             loss = F.cross_entropy(preds, labels) # calculate loss
             loss.backward() # calculate gradients with respect to each weight
             optimizer.step() # update weights
-
-            accuracy = test_nn(net=net, verbose=False)
             
-            # track
-            accuracy_track.append(accuracy)
-            loss_track.append(loss.item())
+            running_loss += loss.item()
+        
+        accuracy = test_nn(net=net, verbose=False)
+            
+        # track
+        accuracy_per_epoch_track.append(accuracy)
+        loss_per_epoch_track.append(running_loss)
 
-        print(f'\nEpoch {epoch} finished')
+        print(f'Epoch: {epoch} -- Loss: {loss_per_epoch_track[-1]} -- Accuracy: {accuracy_per_epoch_track[-1]}')
 
     # plot
     fig, ax1 = plt.subplots()
 
-    ax1.set_xlabel('Evaluations of the loss function')
+    ax1.set_xlabel('Iterations over the entire dataset (Epoch)')
     
     ax1.set_ylabel('Accuracy', color='b')
-    ax1.plot(np.array(accuracy_track), color='b', marker='.', label='Accuracy', linewidth=0.5)
+    ax1.plot(np.array(accuracy_per_epoch_track), color='b', marker='.', label='Accuracy', linestyle='dashed', linewidth=0.5)
 
     ax2 = ax1.twinx()
     ax2.set_ylabel('Loss', color='r')
-    ax2.plot(np.array(loss_track), color='r', marker='.', label='Loss', linewidth=0.5)
+    ax2.plot(np.array(loss_per_epoch_track), color='r', marker='.', label='Loss', linestyle='dashed', linewidth=0.5)
 
     fig.tight_layout()
     fig.legend()
